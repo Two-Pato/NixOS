8d144ac692553c7b2422de4044d1bc58d108940c
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2025-06-08 16:39:46 +0200

    Added small changes

--- system/kaede/configuration.nix
+++ system/kaede/configuration.nix
@@ -37,8 +37,9 @@
   security.sudo.enable = true;
 
   # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
   # Networking & Firewall
   systemd.network.enable = true;
