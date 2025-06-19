2969f666cf9a26c486b6824dcf919bf857660832
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2025-06-19 13:35:21 +0200

    Added environment variables

--- var/environment.nix
+++ var/environment.nix
@@ -8,6 +8,10 @@
     HYPRCURSOR_SIZE = "24";
     OZONE_PLATFORM = "wayland";
     ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+    GDK_BACKEND = "wayland";
+    GDK_SCALE = "2";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_QPA_PLATFORM = "wayland";
     LIBVA_DRIVER_NAME = "nvidia";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
