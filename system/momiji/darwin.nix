0979f8cae52b466d8476441be73ac00d8aabe8a8
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2025-06-29 20:34:04 +0200

    Added small changes

--- system/momiji/darwin.nix
+++ system/momiji/darwin.nix
@@ -5,4 +5,13 @@
   system.defaults.dock = {
     autohide = false;
   };
+
+  #system.defaults.NSGlobalDomain = {
+  #  AppleShowAllExtensions = true;
+  #};
+
+  #system.defaults.finder = {
+  #  AppleShowAllExtensions = true;
+  #  FXPreferredViewStyle = "icnv";
+  #};
 }
