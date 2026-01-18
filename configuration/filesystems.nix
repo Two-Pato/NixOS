b4c6c7635b247c2312303993cee97a4bfdb7070a
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2026-01-18 07:55:40 +0100

    Changed filesystems

--- configuration/filesystems.nix
+++ configuration/filesystems.nix
@@ -1,64 +1,56 @@
 { config, lib, pkgs, ... }:
 
+let
+  cifsOptions = [
+    "_netdev"
+    "noauto"
+    "x-systemd.automount"
+    "x-systemd.idle-timeout=60s"
+    "x-systemd.device-timeout=5s"
+    "x-systemd.mount-timeout=5s"
+    "user"
+    "users"
+    "credentials=/home/laurent/.secrets/nas_token"
+    "uid=1000"
+    "gid=100"
+  ];
+in 
 {
-  fileSystems."/mnt/media" = lib.mkIf (lib.elem config.networking.hostName [ "mihari" "kaede" ]) {
-    device = "//10.0.20.31/media";
-    fsType = "cifs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "user"
-      "users"
-      "credentials=/home/laurent/.secrets/nas_token"
-      "uid=1000"
-      "gid=100"
-    ];
-  };
+  fileSystems = lib.mkMerge [
+    {
+      "/home/laurent/.cache/thumbnails" = {
+        fsType = "tmpfs";
+        neededForBoot = false;
+        options = [
+          "size=512M"
+          "uid=1000"
+          "gid=100"
+        ];
+      };
+    }
 
-  fileSystems."/mnt/videogames" = lib.mkIf (config.networking.hostName == "mahiro") {
-    device = "//10.0.20.31/videogames";
-    fsType = "cifs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "user"
-      "users"
-      "credentials=/home/laurent/.secrets/nas_token"
-      "uid=1000"
-      "gid=100"
-    ];
-  };
+    (lib.mkIf (lib.elem config.networking.hostName [ "mihari" "kaede" ]) {
+      "/mnt/media" = {
+        device = "//10.0.20.31/media";
+        fsType = "cifs";
+        options = cifsOptions;
+      };
+    })
 
-  fileSystems."/mnt/backup" = lib.mkIf (config.networking.hostName == "mihari") {
-    device = "//10.0.20.32/backup";
-    fsType = "cifs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "user"
-      "users"
-      "credentials=/home/laurent/.secrets/nas_token"
-      "uid=1000"
-      "gid=100"
-    ];
-  };
+    (lib.mkIf (config.networking.hostName == "mahiro") {
+      "/mnt/videogames" = {
+        device = "//10.0.20.31/videogames";
+        fsType = "cifs";
+        options = cifsOptions;
+      };
+    })
 
-  fileSystems."/home/laurent/.cache/thumbnails" = {
-    fsType = "tmpfs";
-    neededForBoot = false;
-    options = [
-      "size=512M"
-      "uid=1000"
-      "gid=100"
-    ];
-  };
+    (lib.mkIf (config.networking.hostName == "mihari") {
+      "/mnt/backup" = {
+        device = "//10.0.20.32/backup";
+        fsType = "cifs";
+        options = cifsOptions;
+      };
+    })
+  ];
 }
