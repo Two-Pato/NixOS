8217211e5c6700d1304225032776d8e8c986a18e
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2026-01-22 18:55:36 +0100

    Changed mkvconvert

--- var/scripts/mkvconvert.nix
+++ var/scripts/mkvconvert.nix
@@ -1,10 +1,37 @@
 { pkgs }:
 
-pkgs.writeShellScriptBin "mkvconvert" ''
-  find . -type f -name '*.mkv' | {
-    while read filename ; do
+pkgs.writeShellScriptBin "mkv-convert" ''
+  mkdir -p mux
+
+  find . -type f -name '*.mkv' | while read -r filename; do
+    # Count audio tracks
+    audio_count="$(${pkgs.mkvtoolnix}/bin/mkvmerge -i "$filename" \
+      | grep 'Track ID' \
+      | grep 'audio' \
+      | wc -l)"
+
+    if [ "$audio_count" -ge 2 ]; then
+      # Two soundtracks + English (or similar)
       ${pkgs.mkvtoolnix}/bin/mkvmerge \
-        --output "mux/''${filename}" \
+        --output "mux/$filename" \
+        --language 0:ja \
+        --track-name '0:Video' \
+        --default-track 0:yes \
+        --language 1:ja \
+        --track-name '1:Japanese' \
+        --default-track 1:yes \
+        --language 2:ja \
+        --track-name '2:Japanese Surround' \
+        --default-track 2:no \
+        --language 3:en \
+        --track-name '3:English' \
+        --default-track 3:yes \
+        '(' "$filename" ')' \
+        --track-order 0:0,0:1,0:2,0:3
+    else
+      # Single Japanese + English
+      ${pkgs.mkvtoolnix}/bin/mkvmerge \
+        --output "mux/$filename" \
         --language 0:ja \
         --track-name '0:Video' \
         --default-track 0:yes \
@@ -14,8 +41,8 @@ pkgs.writeShellScriptBin "mkvconvert" ''
         --language 2:en \
         --track-name '2:English' \
         --default-track 2:yes \
-        '(' "''${filename}" ')' \
+        '(' "$filename" ')' \
         --track-order 0:0,0:1,0:2
-    done
-  }
+    fi
+  done
 ''
