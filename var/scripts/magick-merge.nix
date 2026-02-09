5ab3340f0631a33a2a89e401a375a588e19f520a
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2026-02-09 11:06:25 +0100

    Added small changes

--- var/scripts/magick-merge.nix
+++ var/scripts/magick-merge.nix
@@ -4,7 +4,7 @@ pkgs.writeShellScriptBin "magick-merge" ''
   mkdir -p merged
 
   for f in *.jpg; do
-    base="${f%.jpg}"
+    base="''${f%.jpg}"
     if [ -f "$base.png" ]; then
       magick "$f" "$base.png" -gravity south -compose over -composite "merged/$base.png"
     else
