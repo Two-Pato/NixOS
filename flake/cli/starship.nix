b176cf0dd36e4b1cd6229375686c3834d1f2ffda
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2026-03-30 15:11:45 +0200

    Changed starship

--- flake/cli/starship.nix
+++ flake/cli/starship.nix
@@ -1,64 +1,66 @@
-let
-  color = import ../../../var/color.nix;
-in
-{
-  flake.homeModules.cli = {
+{ config, ... }: {
+  flake.homeModules.starship = { ... }: {
     programs.starship = {
       enable = true;
       enableBashIntegration = true;
       enableZshIntegration = true;
+
       settings = {
         format = builtins.concatStringsSep "" [
-          "[](fg:#${color.base08-hex})"
+          "[](fg:#${config.colors.base08-hex})"
           "$username"
-          "[](fg:#${color.base08-hex} bg:#${color.base09-hex})"
+          "[](fg:#${config.colors.base08-hex} bg:#${config.colors.base09-hex})"
           "$hostname"
-          "[](fg:#${color.base09-hex} bg:#${color.base0B-hex})"
+          "[](fg:#${config.colors.base09-hex} bg:#${config.colors.base0B-hex})"
           "$directory"
-          "[](fg:#${color.base0B-hex} bg:#${color.base0D-hex})"
+          "[](fg:#${config.colors.base0B-hex} bg:#${config.colors.base0D-hex})"
           "$git_branch$git_status"
-          "[ ](fg:#${color.base0D-hex})"
+          "[ ](fg:#${config.colors.base0D-hex})"
           "$line_break$character"
         ];
+
         username = {
           disabled = false;
           show_always = true;
           format = "[ $user  ]($style)";
-          style_user = "fg:#${color.base06-hex} bg:#${color.base08-hex}";
-          style_root = "fg:#${color.base06-hex} bg:#${color.base08-hex}";
+          style_user = "fg:#${config.colors.base06-hex} bg:#${config.colors.base08-hex}";
+          style_root = "fg:#${config.colors.base06-hex} bg:#${config.colors.base08-hex}";
         };
+
         hostname = {
           disabled = false;
           ssh_only = false;
           ssh_symbol = "";
           format = "[ $hostname 󰟀 ]($style)";
-          style = "fg:#${color.base06-hex} bg:#${color.base09-hex}";
+          style = "fg:#${config.colors.base06-hex} bg:#${config.colors.base09-hex}";
         };
+
         directory = {
           disabled = false;
           truncation_length = 3;
           truncation_symbol = "…/";
           format = "[ $path ]($style)";
-          style = "fg:#${color.base06-hex} bg:#${color.base0B-hex}";
+          style = "fg:#${config.colors.base06-hex} bg:#${config.colors.base0B-hex}";
         };
+
         git_branch = {
           disabled = false;
           symbol = "";
           format = "[ $symbol $branch ]($style)";
-          style = "fg:#${color.base06-hex} bg:#${color.base0D-hex}";
+          style = "fg:#${config.colors.base06-hex} bg:#${config.colors.base0D-hex}";
         };
+
         git_status = {
           disabled = false;
           format = "[($all_status$ahead_behind)]($style)";
-          style = "fg:#${color.base06-hex} bg:#${color.base0D-hex}";
+          style = "fg:#${config.colors.base06-hex} bg:#${config.colors.base0D-hex}";
         };
-        line_break = {
-          disabled = false;
-        };
+
+        line_break.disabled = false;
         character = {
           disabled = false;
-          success_symbol = "[](bold fg:#${color.base0B-hex})";
-          error_symbol = "[](bold fg:#${color.base08-hex})";
+          success_symbol = "[](bold fg:#${config.colors.base0B-hex})";
+          error_symbol = "[](bold fg:#${config.colors.base08-hex})";
         };
       };
     };
