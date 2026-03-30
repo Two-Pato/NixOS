6fa63bf2b8c45878927b2319339d65aa2dd15eca
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2026-03-30 23:41:07 +0200

    Added home-manger

--- modules/hosts/mihari/flake.nix
+++ modules/hosts/mihari/flake.nix
@@ -6,25 +6,7 @@
       config.flake.nixosModules.core
       config.flake.nixosModules.nixos-base
       config.flake.nixosModules.boot-efi
-      inputs.home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.backupFileExtension = "backup";
-        home-manager.users.laurent = {
-          imports = [
-            config.flake.homeModules.mihari
-            config.flake.homeModules.cli
-            config.flake.homeModules.core
-            config.flake.homeModules.nixos-core
-            config.flake.homeModules.nixos-base
-            config.flake.homeModules.nixos-extra
-            config.flake.homeModules.nixos-desktop
-            inputs.stylix.homeModules.stylix
-            inputs.nvf.homeManagerModules.default
-          ];
-        };
-      }
+      config.flake.nixosModules.home-manager
     ];
   };
 }
