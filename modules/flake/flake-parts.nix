759a55a287a32cf6deeec0b10ca0150c90a1a9bc
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2026-03-31 02:40:40 +0200

    Added small changes

--- modules/flake/flake-parts.nix
+++ modules/flake/flake-parts.nix
@@ -1,15 +1,15 @@
-{ inputs, ... }: {
+{ inputs, lib, ... }: {
   imports = [
     inputs.home-manager.flakeModules.home-manager
-  ];
-
-  systems = [
-    "x86_64-linux"
-    "aarch64-darwin"
   ];
 
   options.flake.darwinModules = lib.mkOption {
     type = lib.types.lazyAttrsOf lib.types.deferredModule;
     default = {};
   };
+
+  config.systems = [
+    "x86_64-linux"
+    "aarch64-darwin"
+  ];
 }
