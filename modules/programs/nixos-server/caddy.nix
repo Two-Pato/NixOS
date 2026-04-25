3287302549d5b3e8ade3746ce03af460fa9fb2ba
Two-Pato <188864485+Two-Pato@users.noreply.github.com>
2026-04-25 06:50:53 +0200

    Added small changes

--- modules/programs/nixos-server/caddy.nix
+++ modules/programs/nixos-server/caddy.nix
@@ -5,7 +5,7 @@
 
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
-        hash = "sha256-Pb27UcjTRfGCmcCAvSZtaXNPANyeH46MeCw3APPv9uI=";
+        hash = "sha256-pt4jyNcfacZKxzRH7zW7l2/+YfmVKWxGD4JTyWpvD1E=";
       };
 
       virtualHosts."adguard.nexuinque.de".extraConfig = ''
